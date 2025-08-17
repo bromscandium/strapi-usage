723fff11b7b0494d2bb4d38ead10c568ddc569a9
User <yaroslav.yeromenko@student.tuke.sk>
2025-08-17 18:37:10 +0200

    init front

--- next-frontend/next.config.mjs
+++ next-frontend/next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
     i18n: {
         locales: ['uk', 'en'],
-        defaultLocale: 'en',
+        defaultLocale: 'uk',
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '1337',
+                pathname: '/uploads/**',
+            },
+        ],
     },
 };
+
+export default nextConfig;
